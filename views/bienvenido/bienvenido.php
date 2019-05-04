07841cd165bde43594b58feeac2aea23e93f9c46
Juan1007 <juanpa1539@gmail.com>
2019-05-03 21:30:03 -0500

    1° Version Proyecto ATH-03052019

--- views/bienvenido/bienvenido.php
+++ views/bienvenido/bienvenido.php
@@ -1,18 +1,46 @@
+<style>
+    h1{
+        font-size:155px};   
+</style>
+<style>
+     h1{
+        font-family: 'BioRhyme Expanded', serif;
+        font-family: 'Indie Flower', cursive;
+        font-family: 'Courgette', cursive;
+        font-family: 'Merienda', cursive;
+        font-family: 'Handlee', cursive;
+</style>
+<style>
+    i{}
+</style>
 <html>
     <head>
         <title>AmoblandoTuHogar</title>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme+Expanded|Courgette|Handlee|Indie+Flower|Merienda" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"> 
     </head>
     <body>
-        <div class="container">
+        <div class="container" style="background-color:#F2EADC;">
             <div class="row">
                 <div class="col-md-12">
                     <br>
                     <br>
                     <br>                    
                     <center><strong><h1>BIENVENIDOS...</h1></strong></center>
+                </div> 
+                <div class="contenedor">
+                    <div class="row">
+                    <div class="col-md-6">
+                        <center><i class="fas fa-child fa-7x"></i></center>
+                    </div>
+                    <div class="col-md-6">
+                        <center><i class="fas fa-child fa-7x"></i></center>
+                    </div>
+                    </div>    
                 </div>
-            </div>
+            </div>           
         </div>
+        
 
     </body>
 </html>
