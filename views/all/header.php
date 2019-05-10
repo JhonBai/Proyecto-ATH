7d305d9e0ae566cb221e9a3c00eae50acbb5938c
Juan1007 <juanpa1539@gmail.com>
2019-05-09 21:30:37 -0500

    3° Version Proyecto ATH-09052019

--- views/all/header.php
+++ views/all/header.php
@@ -6,63 +6,36 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head>
     <body>
-    <div class="contenedor"style="background-color: #f2eadc;">
-        <div class="row">
-            <div class="col-md-3" >
-                <img src="assets/img/logo.png"
+        <div class="container-fluid" style="background-color: #f2eadc;">
+            <div class="row">
+                <div class="col-md-3">
+                   <img src="assets/img/logo.png"   
+                </div>
             </div>
-            
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Inicio</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" >
-            <ul class="navbar-nav">
-            <a class="navbar-brand" href="#">Quienes Somos</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" >
-            <ul class="navbar-nav">
-            <a class="navbar-brand" href="#">Productos</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" >
-            <ul class="navbar-nav">
-                </ul>
-            <a class="navbar-brand" href="#">Iniciar Sesion</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" >
-            <ul class="navbar-nav">
-                </ul>
-            <a class="navbar-brand" href="#">Resgistrate</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" >
-            <ul class="navbar-nav">
-                </ul>
-             <a class="navbar-brand" href="#">Contáctenos</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" >
-            <ul class="navbar-nav">
-            </ul>    
+            <div class="col-md-3">
                 
-    </ul>
-  </div>
-</nav>
+            </div>
+         
+            <div class="col-md-6" style="margin-top: 105px;">                
+                <nav class="navbar-brand" >
+                    <ul class="navbar-brand">
+                       <a class="navbar-brand" href="?c=cliente&m=home">Inicio</a>           
+                        <a class="navbar-brand" href="#">Quienes Somos</a>
+                        <a class="navbar-brand" href="#">Productos</a>               
+                        <a class="navbar-brand" href="#">Iniciar Sesion</a>                
+                        <a class="navbar-brand" href="#">Resgistrate</a>              
+                        <a class="navbar-brand" href="#">Contáctenos</a> 
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </div>
+ 
             
         
 
-   
 
-
+     
 
 
 
--- views/cliente/home.php
+++ views/cliente/home.php
@@ -1,8 +1,77 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<style>
+     h1{
+        font-family: 'BioRhyme Expanded', serif;
+        font-family: 'Indie Flower', cursive;
+        font-family: 'Courgette', cursive;
+        font-family: 'Merienda', cursive;
+        font-family: 'Handlee', cursive;
+        font-weight: bolder;
+        font-size: 75px !important;
+        color: #5b3924;           
+       }
+</style>
+<style>
+    h2{
+        font-family: 'BioRhyme Expanded', serif;
+        font-family: 'Indie Flower', cursive;
+        font-family: 'Courgette', cursive;
+        font-family: 'Merienda', cursive;
+        font-family: 'Handlee', cursive; 
+        font-size: 65px !important;
+    }
+</style>
+<div class="contenedor" style="margin-top: 100px;">
+    <div class="row">
+        <div class="col-md-12">
+            <center><strong><h1>Ofertas del Mes</h1></strong></center>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="margin-top: 50px;">
+    <div class="row">
+        <div class="col-md-6" style="height: 550px;">
+            <center><img src="assets/img/home.jpg" class="fluid-img"></center>
+        </div>
+        <div class="col-md-6" style="height: 550px; background-color: #F2EADC;">
+            <center><h2 style="margin-top:100px;">Combo Modular Alondra + Sala</h2></center>
+            <br>
+            <br>
+            <br>
+            <center><h1> $ 939.999</h1></center>
+            <br>
+            <center><button><a href="">Comprar Ahora</a></button></center>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6" style="height: 550px;background-color: #F2EADC;">
+            <center><h2 style="margin-top:100px;">Sala Cama Sterimberg</h2></center>  
+          <br>
+          <br>
+          <br>
+          <center><h1> $ 939.999</h1></center>
+          <br>
+          <center><button><a href="">Comprar Ahora</a></button></center>
+        </div>
+        <div class="col-md-6" style="height: 550px;">
+            <center><img src="assets/img/home1.jpg" class="fluid-img"></center><
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6" style="height: 550px;">
+            <center><img src="assets/img/home.jpg" class="fluid-img"></center>
+        </div>
+        <div class="col-md-6" style="height: 550px; background-color: #F2EADC;">
+            <center><h2 style="margin-top:100px;">Combo Modular Alondra + Sala</h2></center>
+            <br>
+            <br>
+            <br>
+            <center><h1> $ 939.999</h1></center>
+            <br>
+            <center><button><a href="">Comprar Ahora</a></button></center>
+        </div>
+    </div>
+</div>
